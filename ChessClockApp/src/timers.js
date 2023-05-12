62b9729664150f1d21aa1b1b3a9601bc8cf0a0ac
Tobias Husy <tobi-husy@hotmail.com>
2023-05-12 08:55:47 +0200

    fixed display issue in the bus

--- ChessClockApp/src/timers.js
+++ ChessClockApp/src/timers.js
@@ -4,7 +4,7 @@ class timers {
 constructor(timerSetups){
 
 this.player1 = new timer(timerSetups.timer1);
-this.player2 = new timer(timerSetups.timer1);
+this.player2 = new timer(timerSetups.timer2);
 
 
 }
