1ba29099687c581c4ec4ed99dfca638420dd6123
Tobias Husy <tobi-husy@hotmail.com>
2024-01-02 14:48:45 +0100

    Some cosmetics

--- ChessClockApp/src/settingScript.js
+++ ChessClockApp/src/settingScript.js
@@ -1,79 +1,92 @@
 
 function validateInput(inputField, minValue, maxValue) {
-    var inputValue = inputField.value.trim(); // Remove leading/trailing spaces
-    var validValue = parseInt(inputValue, 10);
+  var inputValue = inputField.value.trim(); // Remove leading/trailing spaces
+  var validValue = parseInt(inputValue, 10);
 
-    // Use a regular expression to ensure that the input is a valid number
-    var validNumber = /^[0-9]+$/.test(inputValue);
+  // Use a regular expression to ensure that the input is a valid number
+  var validNumber = /^[0-9]+$/.test(inputValue);
 
-    if (validNumber) {
-        if (validValue < minValue) {
-            validValue = minValue;
-        } else if (validValue > maxValue) {
-            validValue = maxValue;
-        }
-    } else {
-        // Handle non-numeric or empty input
-        validValue = 0;
+  if (validNumber) {
+    if (validValue < minValue) {
+      validValue = minValue;
+    } else if (validValue > maxValue) {
+      validValue = maxValue;
     }
+  } else {
+    // Handle non-numeric or empty input
+    validValue = 0;
+  }
 
-    // Update the input field with the validated value
-    inputField.value = validValue;
+  // Update the input field with the validated value
+  inputField.value = validValue;
 }
 
 function toggleField(checkbox, field) {
 
-    var hiddenField = document.getElementById(field);
+  var hiddenField = document.getElementById(field);
 
-    if (checkbox.checked) {
-        hiddenField.style.display = "block";
-    } else {
-        hiddenField.style.display = "none";
-    }
+  if (checkbox.checked) {
+    hiddenField.style.display = "block";
+  } else {
+    hiddenField.style.display = "none";
+  }
 }
 
 // Function to check initial stats to togle option fields 
 function checkInitialStates(...divIds) {
-    const initialStates = {};
-    divIds.forEach((divId) => {
-        const checkbox = document.getElementById(divId);
-        toggleField(checkbox, divId + "Time")
-    });
+  const initialStates = {};
+  divIds.forEach((divId) => {
+    const checkbox = document.getElementById(divId);
+    toggleField(checkbox, divId + "Time")
+  });
+
+}
+
+function writeDefaultValues() {
 
 }
 
 function useSettingsButton() {
-    // Daten sammeln und speichern
-    var settingsData = collectSettingsData(document.getElementById('setting'));
-    localStorage.setItem('chessTimerSettings', JSON.stringify(settingsData));
-    window.location.href = 'index.html';
+  // Daten sammeln und speichern
+  var settingsData = collectSettingsData(document.getElementById('setting'));
+  localStorage.setItem('chessTimerSettings', JSON.stringify(settingsData));
+  window.location.href = 'index.html';
 }
 
 function collectSettingsData(parentElement) {
-    var data = {};
+  var data = {};
 
-    // Durchlaufe alle Kinder des übergebenen Elements
-    for (var i = 0; i < parentElement.children.length; i++) {
-      var childElement = parentElement.children[i];
+  // Durchlaufe alle Kinder des übergebenen Elements
+  for (var i = 0; i < parentElement.children.length; i++) {
+    var childElement = parentElement.children[i];
 
-      // Wenn das Kind ein Input-Feld ist
-      if (childElement.tagName === 'INPUT') {
-        if (childElement.type === 'text') {
-          data[childElement.id] = childElement.value;
-        } else if (childElement.type === 'checkbox') {
-          data[childElement.id] = childElement.checked;
-        }
-      }
-
-      // Wenn das Kind ein Container ist, rufe die Funktion rekursiv auf
-      if (childElement.children.length > 0) {
-        var childData = collectSettingsData(childElement);
-        Object.assign(data, childData);
+    // Wenn das Kind ein Input-Feld ist
+    if (childElement.tagName === 'INPUT') {
+      if (childElement.type === 'text') {
+        data[childElement.id] = childElement.value;
+      } else if (childElement.type === 'checkbox') {
+        data[childElement.id] = childElement.checked;
       }
     }
 
-    return data;
+    // Wenn das Kind ein Container ist, rufe die Funktion rekursiv auf
+    if (childElement.children.length > 0) {
+      var childData = collectSettingsData(childElement);
+      Object.assign(data, childData);
+    }
   }
+
+  return data;
+}
 
 // Use the function to check the initial states
 checkInitialStates("fisher1", "delay1", "player2", "fisher2", "delay2");
+
+// Write default Values
+
+writeDefaultValues();
+
+
+window.onload = function () {
+
+}
