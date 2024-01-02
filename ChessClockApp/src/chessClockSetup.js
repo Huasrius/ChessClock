80a521c422542953d28a48dc3e10dcaae3042c43
Tobias Husy <tobi-husy@hotmail.com>
2024-01-02 14:00:44 +0100

    small bug fix

--- ChessClockApp/src/chessClockSetup.js
+++ ChessClockApp/src/chessClockSetup.js
@@ -10,14 +10,14 @@ class chessClockSetupClass {
 
     useNewSetup(chessTimerSettings) {
 
-        this.player1Setup.mainTimeMS = (chessTimerSettings.main1MinutesInput * 60 + chessTimerSettings.main1MinutesInput) * 1000;
+        this.player1Setup.mainTimeMS = (chessTimerSettings.main1MinutesInput * 60 + chessTimerSettings.main1SecondsInput) * 1000;
         this.player1Setup.fisher = chessTimerSettings.fisher1;
         this.player1Setup.fisherTimeMS = (chessTimerSettings.fisher1MinutesInput * 60 + chessTimerSettings.fisher1SecondsInput) * 1000;
         this.player1Setup.delay = chessTimerSettings.delay1;
         this.player1Setup.delayTimeMS = (chessTimerSettings.delay1MinutesInput * 60 + chessTimerSettings.delay1SecondsInput) * 1000;
 
         if (chessTimerSettings.player2) {
-            this.player2Setup.mainTimeMS = (chessTimerSettings.main2MinutesInput * 60 + chessTimerSettings.main2MinutesInput) * 1000;
+            this.player2Setup.mainTimeMS = (chessTimerSettings.main2MinutesInput * 60 + chessTimerSettings.main2SecondsInput) * 1000;
             this.player2Setup.fisher = chessTimerSettings.fisher2;
             this.player2Setup.fisherTimeMS = (chessTimerSettings.fisher2MinutesInput * 60 + chessTimerSettings.fisher2SecondsInput) * 1000;
             this.player2Setup.delay = chessTimerSettings.delay2;
