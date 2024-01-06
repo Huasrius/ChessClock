b31a7e288442ca82f9b7145d0d96d1fdc49b1901
Tobias Husy <tobi-husy@hotmail.com>
2024-01-06 20:02:24 +0100

    bug fix

--- src/settingScript.js
+++ src/settingScript.js
@@ -102,7 +102,11 @@ function collectSettingsData(parentElement) {
     // Wenn das Kind ein Input-Feld ist
     if (childElement.tagName === 'INPUT') {
       if (childElement.type === 'text') {
-        data[childElement.id] = parseInt(childElement.value);
+        if(childElement.value === 'null'){
+          data[childElement.id] = 0;
+        } else {
+          data[childElement.id] = parseInt(childElement.value);
+        }
       } else if (childElement.type === 'checkbox') {
         data[childElement.id] = childElement.checked;
       }
